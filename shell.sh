8bba0f65930d78bdc509ff58a06d3b83ed763fe5
Stelan Briyan <stelan.slt@gmail.com>
2021-05-23 20:17:20 +0530

    shell

--- shell.sh
+++ shell.sh
@@ -1,4 +1,13 @@
+#!/bin/sh
+
+echo "sudo ps -ef | grep demo | grep -v grep | awk '{print $2}' | xargs kill"
 sudo ps -ef | grep demo | grep -v grep | awk '{print $2}' | xargs kill
+
+echo "mvn clean"
 mvn clean
+
+echo "mvn package"
 mvn package
+
+echo "sudo env SERVER.PORT=8081 nohup java -jar target/demo-0.0.1-SNAPSHOT.jar &"
 sudo env SERVER.PORT=8081 nohup java -jar target/demo-0.0.1-SNAPSHOT.jar &
